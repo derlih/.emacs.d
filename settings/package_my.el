05ac2e4423f505f96844f9bd60e82db105b31723
Dmitry Erlikh <derlih@gmail.com>
2020-10-22 10:18:11 +0200

    remove unneeded comment

--- settings/package_my.el
+++ settings/package_my.el
@@ -11,7 +11,7 @@ which is unsafe because it allows man-in-the-middle attacks.
 There are two things you can do about this warning:
 1. Install an Emacs version that does support SSL and be safe.
 2. Remove this warning from your init file so you won't see it again."))
-  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+
 (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
 (add-to-list 'package-archives (cons "marmalade" (concat proto "://marmalade-repo.org/packages/")) t)
 (add-to-list 'package-archives (cons "org" (concat proto "://orgmode.org/elpa/")) t)
